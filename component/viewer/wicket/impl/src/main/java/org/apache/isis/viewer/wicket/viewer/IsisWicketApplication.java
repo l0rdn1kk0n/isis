d8288f14d8adee97acc44e896115a7bae38615cc
Dan Haywood <danhaywood@apache.org>
2013-09-18 00:17:37 +0100

    ISIS-537: added init code to IsisWicketApp...
    ... from the wicket-bootstrap sample.
    
    However, don't see any CSS/JS being included in the page, so there's obviously more stuff that needs to be done?

--- component/viewer/wicket/impl/src/main/java/org/apache/isis/viewer/wicket/viewer/IsisWicketApplication.java
+++ component/viewer/wicket/impl/src/main/java/org/apache/isis/viewer/wicket/viewer/IsisWicketApplication.java
@@ -28,9 +28,27 @@ import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Module;
+import com.google.javascript.jscomp.CompilationLevel;
 
 import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.markup.html.RenderJavaScriptToFooterHeaderResponseDecorator;
+import de.agilecoders.wicket.core.markup.html.references.BootstrapPrettifyCssReference;
+import de.agilecoders.wicket.core.markup.html.references.BootstrapPrettifyJavaScriptReference;
+import de.agilecoders.wicket.core.markup.html.references.ModernizrJavaScriptReference;
+import de.agilecoders.wicket.core.request.resource.caching.version.Adler32ResourceVersion;
 import de.agilecoders.wicket.core.settings.BootstrapSettings;
+import de.agilecoders.wicket.core.settings.ThemeProvider;
+import de.agilecoders.wicket.extensions.javascript.GoogleClosureJavaScriptCompressor;
+import de.agilecoders.wicket.extensions.javascript.YuiCssCompressor;
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.html5player.Html5PlayerCssReference;
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.html5player.Html5PlayerJavaScriptReference;
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.OpenWebIconsCssReference;
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.jqueryui.JQueryUIJavaScriptReference;
+import de.agilecoders.wicket.less.BootstrapLess;
+import de.agilecoders.wicket.themes.markup.html.google.GoogleTheme;
+import de.agilecoders.wicket.themes.markup.html.metro.MetroTheme;
+import de.agilecoders.wicket.themes.markup.html.wicket.WicketTheme;
+import de.agilecoders.wicket.themes.settings.BootswatchThemeProvider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,6 +63,13 @@ import org.apache.wicket.guice.GuiceComponentInjector;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.Request;
 import org.apache.wicket.request.Response;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.request.resource.caching.FilenameWithVersionResourceCachingStrategy;
+import org.apache.wicket.request.resource.caching.NoOpResourceCachingStrategy;
+import org.apache.wicket.request.resource.caching.version.CachingResourceVersion;
+import org.apache.wicket.serialize.java.DeflatedJavaSerializer;
+import org.apache.wicket.settings.IRequestCycleSettings;
 import org.apache.wicket.settings.IRequestCycleSettings.RenderStrategy;
 
 import org.apache.isis.core.commons.authentication.AuthenticationSession;
@@ -223,13 +248,88 @@ public class IsisWicketApplication extends AuthenticatedWebApplication implement
         initWicketComponentInjection(injector);
         
         
-        BootstrapSettings settings = new BootstrapSettings();
-        // ISIS-537: review, this didn't compoile
-        // settings.minify(true); // use minimized version of all bootstrap references
+        // deactivate ajax debug mode
+        getDebugSettings().setAjaxDebugModeEnabled(false);
 
-        Bootstrap.install(this, settings);
+        configureBootstrap();
+        configureResourceBundles();
+
+        optimizeForWebPerformance();
     }
     
+    
+    private void configureBootstrap() {
+        final ThemeProvider themeProvider = new BootswatchThemeProvider() {{
+            add(new MetroTheme());
+            add(new GoogleTheme());
+            add(new WicketTheme());
+            defaultTheme("wicket");
+        }};
+
+        final BootstrapSettings settings = new BootstrapSettings();
+        settings.setJsResourceFilterName("footer-container")
+                .setThemeProvider(themeProvider);
+        Bootstrap.install(this, settings);
+
+        BootstrapLess.install(this);
+    }
+
+    /**
+     * configure all resource bundles (css and js)
+     */
+    private void configureResourceBundles() {
+        getResourceBundles().addJavaScriptBundle(IsisWicketApplication.class, "core.js",
+                                                 (JavaScriptResourceReference) getJavaScriptLibrarySettings().getJQueryReference(),
+                                                 (JavaScriptResourceReference) getJavaScriptLibrarySettings().getWicketEventReference(),
+                                                 (JavaScriptResourceReference) getJavaScriptLibrarySettings().getWicketAjaxReference(),
+                                                 (JavaScriptResourceReference) ModernizrJavaScriptReference.INSTANCE
+        );
+
+//        getResourceBundles().addJavaScriptBundle(IsisWicketApplication.class, "bootstrap.js",
+//                                                 (JavaScriptResourceReference) Bootstrap.getSettings().getJsResourceReference(),
+//                                                 (JavaScriptResourceReference) BootstrapPrettifyJavaScriptReference.INSTANCE,
+//                                                 ApplicationJavaScript.INSTANCE
+//        );
+
+        getResourceBundles().addJavaScriptBundle(IsisWicketApplication.class, "bootstrap-extensions.js",
+                                                 JQueryUIJavaScriptReference.instance(),
+                                                 Html5PlayerJavaScriptReference.instance()
+        );
+
+        getResourceBundles().addCssBundle(IsisWicketApplication.class, "bootstrap-extensions.css",
+                                          Html5PlayerCssReference.instance(),
+                                          OpenWebIconsCssReference.instance()
+        );
+
+//        getResourceBundles().addCssBundle(IsisWicketApplication.class, "application.css",
+//                                          (CssResourceReference) BootstrapPrettifyCssReference.INSTANCE,
+//                                          FixBootstrapStylesCssResourceReference.INSTANCE
+//        );
+    }
+    
+    /**
+     * optimize wicket for a better web performance
+     */
+    private void optimizeForWebPerformance() {
+        if (usesDeploymentConfig()) {
+            getResourceSettings().setCachingStrategy(new FilenameWithVersionResourceCachingStrategy(
+                    "-v-",
+                    new CachingResourceVersion(new Adler32ResourceVersion())
+            ));
+
+            getResourceSettings().setJavaScriptCompressor(new GoogleClosureJavaScriptCompressor(CompilationLevel.SIMPLE_OPTIMIZATIONS));
+            getResourceSettings().setCssCompressor(new YuiCssCompressor());
+
+            getFrameworkSettings().setSerializer(new DeflatedJavaSerializer(getApplicationKey()));
+        } else {
+            getResourceSettings().setCachingStrategy(new NoOpResourceCachingStrategy());
+        }
+
+        setHeaderResponseDecorator(new RenderJavaScriptToFooterHeaderResponseDecorator());
+        getRequestCycleSettings().setRenderStrategy(IRequestCycleSettings.RenderStrategy.ONE_PASS_RENDER);
+    }
+
+
     private void determineDeploymentTypeIfRequired() {
         if(deploymentType != null) {
             return;
